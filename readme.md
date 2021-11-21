07ae28412476315620ebb5c721e0eada99cc5bd5
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2021-11-21 15:05:40 +0700

    Membuat Level 1

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Math Game
+
+Apakah reaksimu ketika mendengar Matematika? Mendebarkan atau Menyenangkan? Nahh, di game ini, kalian bakal merasakan sensasi menyenangkan dan mendebarkan sekaligus. Penasaran, kan? [Klik link ini untuk memulai](https://www.google.com)
+
+## Gameplay
+
+Tinggal klak-klik gitu aja kok.
+
+## Informasi
+
+Game ini belum siap rilis cuyy, yang sabar yo. Tunggu beberapa minggu lagi.
+
+## Siapa yang Buat?
+
+Kami dari tim Pebeka 13 dan berikut ini adalah membernya.
+
+1. Atikah Khonsa Salsabila (1313619003)
+2. Ecep Jiyaul Rojak (1313619009)
+3. Ferdy Nugraha Rachmadi (1313619011)
+4. Muhammad Zhafran Bahij (1313619012)
+5. Handini Aprillia Wijaya (1313619038)
+6. Rahma Wati Malawat (1313619042)
---
+++ script.js
@@ -0,0 +1,68 @@
+//* JUMLAH BENER
+let theTruth = 0;
+let count = 0;
+let score = document.querySelector("#score");
+score.innerHTML = theTruth;
+
+//* NGURUS SOAL
+
+let questions = [
+  ["5 + 6", "3 + 2", "more"],
+  ["4 + 2", "2 + 4", "equals"],
+  ["4 + 7", "7 + 5", "less"],
+
+  ["6 - 3", "5 - 3", "more"],
+  ["6 - 3", "7 - 4", "equals"],
+  ["4 - 3", "4 - 1", "less"],
+
+  ["9 - 3", "9 - 4", "more"],
+  ["8 - 5", "8 - 5", "equals"],
+  ["4 - 4", "4 - 3", "less"],
+];
+
+let quest01 = document.querySelector("#quest01");
+let quest02 = document.querySelector("#quest02");
+let endQuest = document.querySelector("#end_question");
+
+let question = questions[Math.floor(Math.random() * 3)];
+
+quest01.innerHTML = question[0];
+quest02.innerHTML = question[1];
+
+//* NGURUS BUTTON
+
+let buttonLess = document.querySelector("#less");
+let buttonEquals = document.querySelector("#equals");
+let buttonMore = document.querySelector("#more");
+
+const clickButton = (operator) => {
+  console.log(operator);
+  if (question[2] == operator) {
+    theTruth++;
+    console.log("Kau Benar");
+  } else {
+    console.log("Kau Salah");
+  }
+  question = questions[Math.floor(Math.random() * 9)];
+
+  quest01.innerHTML = question[0];
+  quest02.innerHTML = question[1];
+  score.innerHTML = theTruth;
+  // ++count;
+  if (++count == 5) {
+    document.querySelector("#question_menu").classList = "visually-hidden";
+    endQuest.innerHTML = `Score kamu adalah ${theTruth}`;
+  }
+};
+
+buttonLess.addEventListener("click", () => {
+  clickButton("less");
+});
+
+buttonEquals.addEventListener("click", () => {
+  clickButton("equals");
+});
+
+buttonMore.addEventListener("click", () => {
+  clickButton("more");
+});
