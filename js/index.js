112ea166cf73aa854409a0802efd90d716236616
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2021-12-01 16:21:14 +0700

    Nyoba betulin bug di level

--- js/index.js
+++ js/index.js
@@ -4,12 +4,21 @@ let level02 = document.querySelector("#level02");
 let level03 = document.querySelector("#level03");
 let level04 = document.querySelector("#level04");
 
-// setInterval(() => {
-//   let whatLevel = localStorage.getItem("Level_LP");
-//   if (whatLevel >= 2) {
-//     level02.className = "btn";
-//   }
-// }, 1000);
+if (typeof Storage !== "undefined") {
+  // Code for localStorage/sessionStorage.
+  // localStorage.setItem("number", "0");
+  // pengecekkan apakah data sessionStorage dengan key NUMBER tersedia atau belum
+  if (localStorage.getItem("Level_LP") === "undefined") {
+    // Jika belum maka akan atur dengan nilai awal yakni 0
+    localStorage.setItem("Level_LP", 0);
+  }
+  var count = localStorage.getItem("Level_LP");
+  setInterval(() => {
+    localStorage.setItem(cacheKey, count);
+  }, 100);
+} else {
+  // Sorry! No Web Storage support..
+}
 
 //* Unlock Level
 let whatLevel = localStorage.getItem("Level_LP");
