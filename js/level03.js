fe7207d0b6066cbe06a40d2786f825428e26b3e3
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2021-12-06 14:03:12 +0700

    Menambahkan level 4, tetapi untuk sementara waktu, dipasang di level 3

---
+++ js/level03.js
@@ -0,0 +1,35 @@
+var questions = {
+  data: [
+    //  PANGKAT DUA DAN TIGA
+    ["6<sup>2</sup>", "2<sup>2</sup>", "more"],
+    ["8<sup>2</sup>", "4<sup>3</sup>", "equals"],
+    ["3<sup>2</sup>", "3<sup>3</sup>", "less"],
+
+    ["7<sup>3</sup>", "5<sup>3</sup>", "more"],
+    ["8<sup>2</sup>", "8<sup>2</sup>", "equals"],
+    ["9<sup>3</sup>", "5<sup>2</sup>", "less"],
+
+    ["2<sup>3</sup> x 3<sup>3</sup>", "4<sup>2</sup> x 2<sup>2</sup>", "more"],
+    ["8<sup>2</sup> + 2<sup>3</sup>", "8 x 9", "equals"],
+    ["9<sup>3</sup>", "8<sup>3</sup> - 3<sup>3</sup>", "less"],
+
+    ["9<sup>2</sup>", "7<sup>2</sup>", "more"],
+    ["9 x 3", "3<sup>3</sup>", "equals"],
+    ["6 x 5", "4<sup>3</sup>", "less"],
+
+    ["7<sup>3</sup> : 7<sup>2</sup>", "2<sup>3</sup> : 2<sup>2</sup>", "more"],
+    ["5<sup>3</sup> : 5", "5<sup>2</sup>", "equals"],
+    ["9<sup>3</sup>", "9<sup>2</sup>", "more"],
+
+    ["6<sup>2</sup>", "1<sup>2</sup>", "more"],
+    ["4<sup>3</sup> : 4<sup>2</sup>", "2<sup>2</sup>", "equals"],
+    ["6<sup>3</sup>", "7<sup>3</sup>", "less"],
+
+    ["4<sup>2</sup>", "2<sup>2</sup> : 2", "more"],
+    ["5<sup>2</sup>", "5<sup>3</sup>", "less"],
+  ],
+
+  unlock_level: 4,
+  example: "5<sup>3</sup>",
+  description: "5<sup>3</sup> berarti 5 x 5 x 5 = 125. ",
+};
